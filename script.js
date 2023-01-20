cbb0ae290718bbf82e8e86a8ad693dab915b70bd
Mamoon <muhammadmamoon771@gmail.com>
2023-01-20 23:41:50 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+   function calculate() {
+       let userGet = document.querySelector("#get").value;
+       let outoff = document.querySelector("#outoff").value;
+       let resutl = userGet / outoff * 100;
+       let show = document.querySelector("p").innerText = `${resutl} %`;
+   }
+
+   function clear() {
+
+   }
